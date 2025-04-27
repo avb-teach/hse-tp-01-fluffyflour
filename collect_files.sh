6a0cca5a4e28c72d7791139df1539a7c7a46f9f4
fluffyflour <ekarkeshkina@bk.ru>
2025-04-27 20:18:04 +0300

    k

--- collect_files.sh
+++ collect_files.sh
@@ -2,24 +2,3 @@
 
 input_dir="$1"
 output_dir="$2"
-
-mycp() {
-    local source="$1"
-    local target_dir="$2"
-    local filename=$(basename -- "$source")
-    local name="${filename%.*}"
-    local ext="${filename##*.}"
-    local dest_path="$target_dir/$filename"
-    local counter=1
-
-    while [[ -e "$dest_path" ]]; do
-        if [[ "$ext" != "$filename" ]]; then
-            dest_path="$target_dir/${name}_${counter}.${ext}"
-        else
-            dest_path="$target_dir/${name}_${counter}"
-        fi
-        ((counter++))
-    done
-
-   cp -- "$source" "$dest_path"; then
-}
